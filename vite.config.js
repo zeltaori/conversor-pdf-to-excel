702c4bb62ba1ba94762cafc1057a61fda748d971
zeltaori <sebastiao.ront@gmail.com>
2026-07-13 17:03:28 -0300

    Create vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,20 @@
+import path from "path";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  // Troque pelo nome exato do seu repositório no GitHub.
+  base: "/conversor-pdf-excel/",
+
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
+
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
